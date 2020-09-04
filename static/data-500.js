b48e3b2b039000a39220ca18b667a66657669e14
Anastasia Pedashenko <anastasia1188@mail.ru>
2020-09-04 19:07:52 +0300

    2-й спринт (1 ревью)

--- static/data-500.js
+++
@@ -1,6 +0,0 @@
-const elemTemplate = document.getElementById('template-500');
-const source = elemTemplate.innerHTML;
-const template = Handlebars.compile(source);
-const context = { errmes: "Мы уже фиксим", errcode: "500" };
-const html = template(context);
-elemTemplate.innerHTML = html;
