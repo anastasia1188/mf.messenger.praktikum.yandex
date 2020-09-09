465a254d4202402ec3d35cdcca1f1a2d571a2904
Anastasia Pedashenko <anastasia1188@mail.ru>
2020-09-09 10:23:21 +0300

    2 спринт (3 ревью)

--- static/settings/settings.tmpl.js
+++ static/settings/settings.tmpl.js
@@ -1,4 +1,4 @@
-function getGemplateSettings() {
+function getTemplateSettings() {
     return `<div class="basic-wrapper">
     <div class="basic-wrapper__item">
         <div class="basic-wrapper__settings">
