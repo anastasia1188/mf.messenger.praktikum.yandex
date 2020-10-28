f5e3c83a9886d85ef9bb86ebf035ce3567c128d1
Anastasia Pedashenko <anastasia1188@mail.ru>
2020-10-28 20:37:39 +0300

    3

--- utils/renderDOM.js
+++
@@ -1,8 +0,0 @@
-export function render(query, block) {
-    console.log('test', query);
-    const root = document.querySelector(query);
-
-    // Можно завязаться на реализации вашего класса Block
-    root.appendChild(block.getContent());
-    return root;
-}
