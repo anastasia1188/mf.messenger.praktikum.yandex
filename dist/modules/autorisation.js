222ec41f8609d304768a378cb1eceb0f8da795f7
Anastasia Pedashenko <anastasia1188@mail.ru>
2020-11-13 18:13:27 +0300

    3

--- dist/modules/autorisation.js
+++ dist/modules/autorisation.js
@@ -1,43 +1,47 @@
+import HTTPTransport from "../../../dist/modules/httpTransport.js";
 export function autorisation(user) {
-    const host = 'http://mf.messenger.praktikum.yandex';
+    const httpTransport = new HTTPTransport;
+    //const host = 'http://mf.messenger.praktikum.yandex';
+    const host = 'https://amazing-turing-810a80.netlify.app/'
     fetch(`${host}/data/users.json`, {
-        method: 'POST',
-        credentials: 'include',
-        mode: 'cors',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: user.name,
-            login: user.login,
-            email: user.email,
-            password: user.password,
-        }),
-    }).then(response => response.text())
-        .then(data => {
-        console.log(data);
-        return data;
-    })
-        .then(data => {
-        fetch(`${host}/data/users.json`, {
-            method: 'GET',
-            mode: 'cors',
+            method: 'POST',
             credentials: 'include',
+            mode: 'cors',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: user.name,
+                login: user.login,
+                email: user.email,
+                password: user.password,
+            }),
+        }).then(response => response.text())
+        .then(data => {
+            console.log(data);
+            return data;
         })
-            .then(r => r.json())
-            .then(data => {
-            if (data != undefined) {
-                for (let i = 0; i < data.length; i++) {
-                    if ((data[i].login === user.login) && (data[i].password === user.password)) {
-                        user.result = true;
+        .then(data => {
+            fetch(`${host}/data/users.json`, {
+                    method: 'GET',
+                    mode: 'cors',
+                    credentials: 'include',
+                })
+                .then(r => r.json())
+                .then(data => {
+                    if (data != undefined) {
+                        for (let i = 0; i < data.length; i++) {
+                            if ((data[i].login === user.login) && (data[i].password === user.password)) {
+                                user.result = true;
+                            }
+                        }
                     }
-                }
-            }
+                });
         });
-    });
 }
 export async function registration(user) {
-    const host = 'http://mf.messenger.praktikum.yandex';
+    //const host = 'http://mf.messenger.praktikum.yandex';
+    const host = 'https://amazing-turing-810a80.netlify.app/'
     let res = await fetch(`${host}/data/users.json`);
     let resJSON = await res.json();
     if (resJSON.indexOf(user.login) === -1) {
@@ -53,21 +57,21 @@ export async function registration(user) {
             },
             body: body
         });
-    }
-    else
+    } else
         user.result = false;
 }
+
 function postFile(form) {
     const host = 'http://mf.messenger.praktikum.yandex';
     const formData = new FormData(form);
     fetch(`${host}/data/image`, {
-        method: 'PUT',
-        credentials: 'include',
-        mode: 'cors',
-        body: formData,
-    })
+            method: 'PUT',
+            credentials: 'include',
+            mode: 'cors',
+            body: formData,
+        })
         .then(response => response.json())
         .then(data => {
-        return data;
-    });
+            return data;
+        });
 }
