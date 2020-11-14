1f859de30567bb1e944e3f5807cc775ba6cd53f9
Anastasia Pedashenko <anastasia1188@mail.ru>
2020-11-14 22:20:13 +0300

    3 спринт

--- dist/modules/autorisation.js
+++ dist/modules/autorisation.js
@@ -1,57 +1,33 @@
-export function autorisation(user) {
-    //const host = 'http://mf.messenger.praktikum.yandex';
-    const host = 'https://amazing-turing-810a80.netlify.app/';
-    fetch(`${host}/data/users.json`, {
-        method: 'POST',
-        credentials: 'include',
-        mode: 'cors',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: user.name,
-            login: user.login,
-            email: user.email,
-            password: user.password,
-        }),
-    }).then(response => response.text())
-        .then(data => {
-        return data;
-    })
-        .then(data => {
-        fetch(`${host}/data/users.json`, {
-            method: 'GET',
-            mode: 'cors',
-            credentials: 'include',
-        })
-            .then(r => r.json())
-            .then(data => {
-            if (data != undefined) {
-                for (let i = 0; i < data.length; i++) {
-                    if ((data[i].login === user.login) && (data[i].password === user.password)) {
-                        user.result = true;
-                    }
-                }
-            }
-        });
-    });
+import { HTTPTransport } from "../../dist/modules/httpTransport.js";
+export async function autorisation(user) {
+    const host = 'http://mf.messenger.praktikum.yandex';
+    //const host =  'https://amazing-turing-810a80.netlify.app/'
+    const httpTransport = new HTTPTransport;
+    const res = await httpTransport.get(`${host}/data/users.json`);
+    const data = JSON.parse(res.responseText);
+    for (let i = 0; i < data.length; i++) {
+        if ((data[i].login === user.login) && (data[i].password === user.password)) {
+            user.result = true;
+        }
+    }
 }
 export async function registration(user) {
-    //const host = 'http://mf.messenger.praktikum.yandex';
-    const host = 'https://amazing-turing-810a80.netlify.app/';
-    let res = await fetch(`${host}/data/users.json`);
-    let resJSON = await res.json();
+    const host = 'http://mf.messenger.praktikum.yandex';
+    //const host = 'https://amazing-turing-810a80.netlify.app/';
+    const httpTransport = new HTTPTransport;
+    const res = await httpTransport.post(`${host}/data/users.json`);
+    const resJSON = res.responseText;
     if (resJSON.indexOf(user.login) === -1) {
         user.result = true;
         resJSON.push(JSON.stringify(user));
-        let body = JSON.stringify(resJSON);
-        fetch(`${host}/data/users.json`, {
-            method: 'PUT',
+        const body = JSON.stringify(resJSON);
+        const options = {
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: body
-        });
+            body: body,
+        };
+        httpTransport.post('user.json', options);
     }
     else
         user.result = false;
